5eeb0b0dfbbf3e87ce585314b841df2f1c67a412
eksublogs-droid <eksublogs@gmail.com>
2026-07-13 22:13:11 +0100

    Add files via upload

---
+++ public/categories.js
@@ -0,0 +1,37 @@
+const CATEGORIES = [
+  {
+    name: 'Home & Garden',
+    icon: '🏠',
+    subcategories: ['Tools', 'Furniture', 'Garden', 'Appliances', 'Household']
+  },
+  {
+    name: 'Entertainment',
+    icon: '🎬',
+    subcategories: ['Books', 'Movies & Music', 'Video Games']
+  },
+  {
+    name: 'Electronics',
+    icon: '📱',
+    subcategories: ['Mobile Phones', 'Electronics & Computers']
+  },
+  {
+    name: 'Hobbies',
+    icon: '🎸',
+    subcategories: ['Sports & Outdoors', 'Musical Instruments', 'Arts & Crafts', 'Antiques & Collectibles', 'Auto Parts', 'Bicycles']
+  },
+  {
+    name: 'Clothing & Accessories',
+    icon: '👗',
+    subcategories: ['Jewelry & Accessories', 'Bags & Luggage', "Men's Clothing & Shoes", "Women's Clothing & Shoes"]
+  },
+  {
+    name: 'Family',
+    icon: '👨‍👩‍👧',
+    subcategories: ['Toys & Games', 'Baby & Kids', 'Pet Supplies', 'Health & Beauty']
+  },
+  {
+    name: 'Classifieds',
+    icon: '📋',
+    subcategories: ['Garage Sale', 'Miscellaneous']
+  }
+];
